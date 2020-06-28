c027b781e984fb8f86c80744e1794dcc3136a55a
tesselock <33728151+tesselock@users.noreply.github.com>
2020-06-28 13:58:31 -0500

    owo server

--- server/index.js
+++ server/index.js
@@ -4,4 +4,30 @@ const http= require('http');
 
 const app=express();
 
+const PORT=process.env.PORT || 5000;
+//listens for port for deployment, if not deployed, then run locally on port 3000
+
+const router=require('./router');
+//calls the router that we made
+
+//go to socket.io and find the documentation after control+f 'node'
+
 // we use require because we're now dealing with server and backend, not react
+
+const server=http.createServer(app);
+const io=socketio(server);
+
+io.on('connection', (socket) =>{
+  console.log('we 1 socket bois');
+
+  //run all code inside of socket bc we're all just managing one socket
+  socket.on('disconnect',()=>{
+    console.log('sad boi hours :c');
+  });
+});
+
+//pass in app creted from express, and io is passing in server creted
+app.use(router);
+//calls the router
+
+server.listen(PORT,() => console.log(`Server has started kekekkeke ${PORT}`));
