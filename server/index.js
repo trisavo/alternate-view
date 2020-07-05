f44a03f256b73150264a07365cc85f9005e32d66
tesselock <33728151+tesselock@users.noreply.github.com>
2020-07-04 21:40:56 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const socketio=require('socket.io');
 const http= require('http');
+const cors=require('cors');
 
 const {addUser,removeUser,getUser,getUsersInRoom} = require('./users.js');
 
@@ -19,6 +20,8 @@ const router=require('./router');
 const server=http.createServer(app);
 const io=socketio(server);
 
+app.use(router);
+app.use(cors());
 io.on('connection', (socket) =>{
   console.log('we 1 socket bois');
 
